0a83a3a3d3aab1848ea14236002016f1a4991839
Toti Muñoz <totigm@gmail.com>
2022-06-22 20:00:14 -0300

    🚨 Update double quotes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const config = {
-    node_env: process.env.NODE_ENV ?? 'dev',
+    node_env: process.env.NODE_ENV ?? "dev",
 };
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import config from './config';
+import config from "./config";
 
 console.log(`This is the Typescript template at the ${config.node_env} environment`);
